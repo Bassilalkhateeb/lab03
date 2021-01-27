e9d5aff92452823e4210fb61a89102218b7e0cdb
Bassilalkhateeb <basilalkhateeb10@gamil.com>
2021-01-27 22:26:37 +0200

    final edition

--- js.js
+++ js.js
@@ -1,29 +1,35 @@
 var Anime=prompt("What Anime is your fav?");
-var numofshirts=prompt("How many Tshirts you want to order?") ;
-
+var numofshirts=prompt("How many Tshirts you want to order?");
 
 var orderDetails;
 
-if (numofshirts >= 1 && numofshirts <4){
+if (numofshirts >= 1 && numofshirts <= 4){
+
     orderDetails="Congrats you have a discount 25%";
+    confirm("Are you happy with the offer??")
 }
- else if (numofshirts > 4 && numofshirts <=7){
+ else if (numofshirts > 4 && numofshirts <=8){
     orderDetails="Congrats you have a discount 50%";
+    confirm("Are you happy with the offer??")
 }
- else if (numofshirts > 7 && numofshirts <=10){
+ else if (numofshirts > 8 && numofshirts <=12){
     orderDetails="Congrats you have a discount 50% and a Figuer of your fav character";
+    confirm("Are you happy with the offer??")
 }
- else if (numofshirts > 10){
+ else if (numofshirts > 12){
     orderDetails="Congrats you have a discount 50% , a Figuer of your fav character , and a Manga!!!";
+    confirm("Are you happy with the offer??")
 }
 
-else{
-    orderDetails="no discount";
+else {
+    orderDetails="No discount :(";
+    confirm("You have to buy in order to get a discount!")
 }
 
 document.write(orderDetails);
 
-document.getElementById("order").innerText="Num of Tshirts :"+numofshirts;
-document.getElementById("Congrats you have a discount 50% , a Figuer of your fav character , and a Manga!!!");
-confirm("Are you happy with the offer??");
+document.getElementById("order").innerText="Num of Tshirts : "+numofshirts;
+document.getElementById("order2").innerText="Fav Anime : "+Anime;
+
+confirm("Do you live in Amman ?");
 alert("Arigatou");
