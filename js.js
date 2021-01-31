7ef7e35bc42f28f07644605a75d1b9a919349de7
Bassilalkhateeb <basilalkhateeb10@gamil.com>
2021-01-31 16:01:57 +0200

    almost done

--- js.js
+++ js.js
@@ -4,47 +4,56 @@ while (Anime !== 'Aot' && Anime !== 'Bleach'){
 }
 var numofshirts=prompt("How many Tshirts you want to order?");
 
+
+
+
+// Here is my functions //
+function first() {
+    if (numofshirts >= 1 && numofshirts <= 4){
+
+        orderDetails="Congrats you have a discount 25%";
+        confirm("Are you happy with the offer??")
+    }
+     else if (numofshirts > 4 && numofshirts <=8){
+        orderDetails="Congrats you have a discount 50%";
+        confirm("Are you happy with the offer??")
+    }
+     else if (numofshirts > 8 && numofshirts <=12){
+        orderDetails="Congrats you have a discount 50% and a Figuer of your fav character";
+        confirm("Are you happy with the offer??")
+    }
+     else if (numofshirts > 12){
+        orderDetails="Congrats you have a discount 50% , a Figuer of your fav character , and a Manga!!!";
+        confirm("Are you happy with the offer??")
+    }
+    
+    else {
+        orderDetails="No discount :(";
+        confirm("You have to buy in order to get a discount!")
+    }
+}
+first()
 var orderDetails;
 
-if (numofshirts >= 1 && numofshirts <= 4){
 
-    orderDetails="Congrats you have a discount 25%";
-    confirm("Are you happy with the offer??")
-}
- else if (numofshirts > 4 && numofshirts <=8){
-    orderDetails="Congrats you have a discount 50%";
-    confirm("Are you happy with the offer??")
-}
- else if (numofshirts > 8 && numofshirts <=12){
-    orderDetails="Congrats you have a discount 50% and a Figuer of your fav character";
-    confirm("Are you happy with the offer??")
-}
- else if (numofshirts > 12){
-    orderDetails="Congrats you have a discount 50% , a Figuer of your fav character , and a Manga!!!";
-    confirm("Are you happy with the offer??")
-}
-
-else {
-    orderDetails="No discount :(";
-    confirm("You have to buy in order to get a discount!")
-}
 var result = '' ;
 
 var image = ' ';
-if (Anime === 'Aot'){
-    image = '<img src="images/attackon.jpg"/>';
-
-} else if (Anime === 'Bleach'){
-    image = '<img src="images/bleach.jpg"/>';
+function second(){
+    if (Anime === 'Aot'){
+        image = '<img src="images/attackon.jpg"/>';
+    
+    } else if (Anime === 'Bleach'){
+        image = '<img src="images/bleach.jpg"/>';
+    }
+    
+    for (var i = 0 ; i < numofshirts;i++){
+        result = result + image ;
+    }
 }
-
-for (var i = 0 ; i < numofshirts;i++){
-    result = result + image ;
-}
-
+second()
 
 document.write(result);
-
 document.getElementById("order").innerText="Num of Tshirts : "+numofshirts;
 document.getElementById("order2").innerText="Fav Anime : "+Anime;
 document.getElementById("discmsg").innerText=orderDetails;
